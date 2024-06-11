64ba6de0f903de43cc9dd56cdb1cb6e01f1cd105
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-06-11 12:41:20 +0300

    challenge

--- models/author.py
+++ models/author.py
@@ -1,78 +1,53 @@
-from database.connection import get_db_connection
-# retrieval of name property from the database.
-
 class Author:
-  
-    def __init__(self, author_id, name):
+    def __init__(self, id, name):
         self._id = id
-        self.id = author_id
-        self._name = None
-        self.name = name
-
-
+        self._name = name
 
     @property
     def id(self):
         return self._id
-
-    @id.setter
-    def id(self, author_id):
-        if not isinstance(author_id, int):
-            raise TypeError("Author ID must be an integer")
-        else:
-            self._id = author_id
 
     @property
     def name(self):
         return self._name
 
     @name.setter
-    def name(self, name):
-        if hasattr(self, '_name') and self._name is not None:
-            raise AttributeError("Author name has already been set")
-        if not isinstance(name, str) or len(name) == 0:
-            raise TypeError("Author name must be a non-empty string")
-        self._name = name
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string.")
+        if len(value) == 0:
+            raise ValueError("Name must not be empty.")
+        if hasattr(self, '_name'):
+            raise AttributeError("Name cannot be changed after instantiation.")
+        self._name = value
 
-    def add_to_authors():
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        sql = """
-            INSERT INTO authors(name)
-            VALUES (?)
-        """
-        cursor.execute(sql, (self.name,))
-        conn.commit()
-        conn.close()
+    def create_author(self, cursor):
+         # inserting a new author 
+        cursor.execute("INSERT INTO authors (name) VALUES (?)", (self._name,))
+        self._id = cursor.lastrowid
 
-    def articles(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        sql = """
-            SELECT articles.id, articles.title, articles.content, articles.author_id, articles.magazine_id
-            FROM articles
-            JOIN authors ON articles.author_id = authors.id
-            WHERE authors.id =?
-        """
-        cursor.execute(sql, (self.id,))
-        articles = cursor.fetchall()
-        conn.close()
-        return articles
+    @classmethod
+      # getting all authors
+    def get_all_authors(cls, cursor):
+        cursor.execute("SELECT * FROM authors")
+        authors_data = cursor.fetchall()
+        return [cls(id=row[0], name=row[1]) for row in authors_data]
 
-    def magazines(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        sql = """
-            SELECT DISTINCT magazines.id, magazine.name, magazine.category
-            FROM articles
-            JOIN magazines ON articles.magazine_id = magazines.id
-            WHERE articles.author_id =?
-        """
-        cursor.execute(sql, (self.id,))
-        magazines = cursor.fetchall()
-        conn.close()
-        return magazines
+    def articles(self, cursor):
+        # getting all articles associated with a specific author
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self._id,))
+        articles_data = cursor.fetchall()
+        return articles_data
 
+    def magazines(self, cursor):
+         # getting all magazines associated with a specific author
+        cursor.execute("""
+            SELECT magazines.*
+            FROM magazines
+            JOIN articles ON magazines.id = articles.magazine_id
+            WHERE articles.author_id = ?
+        """, (self._id,))
+        magazines_data = cursor.fetchall()
+        return magazines_data
 
-    def __repr__(self):
-        return f'<Author {self.name}>'
+   
